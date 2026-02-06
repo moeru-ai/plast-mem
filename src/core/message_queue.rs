7fe01a03c2ee9663fdac072c62bfd3b0b4c621cc
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-02-06 19:00:12 +0800

    refactor: optimize message queue insert

--- src/core/message_queue.rs
+++ src/core/message_queue.rs
@@ -1,11 +1,11 @@
 use super::Message;
 use crate::utils::AppError;
+use anyhow::anyhow;
 use plast_mem_db_schema::message_queue;
 use sea_orm::{
-  ActiveModelTrait, ColumnTrait, DatabaseConnection, EntityTrait, ExprTrait, QueryFilter, Set,
-  TransactionTrait,
+  ColumnTrait, DatabaseConnection, EntityTrait, ExprTrait, QueryFilter, Set,
   prelude::Expr,
-  sea_query::{BinOper, extension::postgres::PgBinOper},
+  sea_query::{BinOper, OnConflict, extension::postgres::PgBinOper},
 };
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
@@ -27,22 +27,28 @@ impl MessageQueue {
     id: Uuid,
     db: &DatabaseConnection,
   ) -> Result<message_queue::Model, AppError> {
-    let txn = db.begin().await?;
+    if let Some(model) = message_queue::Entity::find_by_id(id).one(db).await? {
+      return Ok(model);
+    }
 
-    let queue = match message_queue::Entity::find_by_id(id).one(&txn).await? {
-      Some(model) => model,
-      None => {
-        let active_model = message_queue::ActiveModel {
-          id: Set(id),
-          messages: Set(serde_json::to_value(Vec::<Message>::new())?),
-        };
-
-        active_model.insert(&txn).await?
-      }
+    let active_model = message_queue::ActiveModel {
+      id: Set(id),
+      messages: Set(serde_json::to_value(Vec::<Message>::new())?),
     };
 
-    txn.commit().await?;
-    Ok(queue)
+    message_queue::Entity::insert(active_model)
+      .on_conflict(
+        OnConflict::column(message_queue::Column::Id)
+          .do_nothing()
+          .to_owned(),
+      )
+      .exec_without_returning(db)
+      .await?;
+
+    message_queue::Entity::find_by_id(id)
+      .one(db)
+      .await?
+      .ok_or_else(|| anyhow!("Failed to ensure queue existence").into())
   }
 
   pub fn from_model(model: message_queue::Model) -> Result<Self, AppError> {
@@ -60,9 +66,9 @@ impl MessageQueue {
   // }
 
   pub async fn push(id: Uuid, message: Message, db: &DatabaseConnection) -> Result<(), AppError> {
-    let message_value = serde_json::to_value(message)?;
+    let message_value = serde_json::to_value(vec![message])?;
 
-    message_queue::Entity::update_many()
+    let res = message_queue::Entity::update_many()
       .col_expr(
         message_queue::Column::Messages,
         Expr::col(message_queue::Column::Messages).binary(
@@ -75,6 +81,10 @@ impl MessageQueue {
       .await?;
 
     // TODO: check segment
+
+    if res.rows_affected == 0 {
+      return Err(anyhow!("Queue not found").into());
+    }
 
     Ok(())
   }
