8e918c00e7b55335ab91dcadf7ab721d5ae429db
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-02-19 23:32:40 +0800

    chore: clean

--- examples/haru/src/hooks/use-terminal-title.ts
+++ examples/haru/src/hooks/use-terminal-title.ts
@@ -2,12 +2,10 @@ import { stdout } from 'node:process'
 
 import { useEffect } from 'react'
 
-export const useTerminalTitle = (title: string) => {
-  useEffect(() => {
-    stdout.write(`\x1B]0;${title}\x07`)
+export const useTerminalTitle = (title: string) => useEffect(() => {
+  stdout.write(`\x1B]0;${title}\x07`)
 
-    return () => {
-      stdout.write('\u001B]0;\u0007')
-    }
-  }, [title])
-}
+  return () => {
+    stdout.write('\u001B]0;\u0007')
+  }
+}, [title])
